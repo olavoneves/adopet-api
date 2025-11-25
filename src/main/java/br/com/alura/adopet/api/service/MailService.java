cb24ba049fa4b1eaacf213e03834e8956015e6c2
Olavo Neves <olavo9neves@gmail.com>
2025-11-24 23:49:50 -0300

    refactor: Adiciona Service para enviar email dinâmico

---
+++ src/main/java/br/com/alura/adopet/api/service/MailService.java
@@ -0,0 +1,22 @@
+package br.com.alura.adopet.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom("adopet@email.com.br");
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(text);
+        mailSender.send(email);
+    }
+}
