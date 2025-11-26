17e8b8374246d37025e331145e9b9f453a7c6a7b
Olavo Neves <olavo9neves@gmail.com>
2025-11-26 19:20:01 -0300

    refactor: Ajusta listar pets disponiveis para melhores praáticas

--- src/main/java/br/com/alura/adopet/api/controller/PetController.java
+++ src/main/java/br/com/alura/adopet/api/controller/PetController.java
@@ -1,7 +1,9 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,18 +18,17 @@ import java.util.List;
 public class PetController {
 
     @Autowired
-    private PetRepository repository;
+    private PetService service;
 
     @GetMapping
     public ResponseEntity<List<Pet>> listarTodosDisponiveis() {
-        List<Pet> pets = repository.findAll();
-        List<Pet> disponiveis = new ArrayList<>();
-        for (Pet pet : pets) {
-            if (pet.getAdotado() == false) {
-                disponiveis.add(pet);
-            }
+        try {
+            List<Pet> pets = service.listarTodosDisponiveis();
+            return ResponseEntity.ok(pets);
+
+        } catch (ValidacaoException exception) {
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(disponiveis);
     }
 
 }
--- src/main/java/br/com/alura/adopet/api/service/PetService.java
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -1,4 +1,19 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PetService {
+
+    @Autowired
+    private PetRepository repository;
+
+    public List<Pet> listarTodosDisponiveis() {
+        return repository.findByAdotado(false);
+    }
 }
