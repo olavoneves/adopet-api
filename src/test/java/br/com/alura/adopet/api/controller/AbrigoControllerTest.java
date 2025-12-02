f62dd40fac1941e7285f5a45925d4b12b7e68bbe
Olavo Neves <olavo9neves@gmail.com>
2025-12-02 19:42:21 -0300

    feat: Adiciona teste para métodos de listagem e cadastro de AbrigoController

---
+++ src/test/java/br/com/alura/adopet/api/controller/AbrigoControllerTest.java
@@ -0,0 +1,85 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.dto.CadastrarAbrigoDTO;
+import br.com.alura.adopet.api.service.AbrigoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class AbrigoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AbrigoService abrigoService;
+
+    @Autowired
+    private JacksonTester<CadastrarAbrigoDTO> cadastrarAbrigoJsonDTO;
+
+    @Test
+    @DisplayName("Deveria retornar status 200 caso liste abrigos")
+    void listarAbrigosCenario01() throws Exception{
+
+        // ARRANGE
+
+        // ACT
+        var response = mvc.perform(
+                get("/abrigos")
+        ).andReturn().getResponse();
+
+        // ASSERT
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Deveria retornar status 200 caso cadastre abrigos")
+    void cadastrarAbrigosCenario01() throws Exception{
+
+        // ARRANGE
+        CadastrarAbrigoDTO dto = new CadastrarAbrigoDTO("Abrigo 2", "11933304765", "abrigo02@abrigo.com");
+
+        // ACT
+        var response = mvc.perform(
+                post("/abrigos")
+                        .content(cadastrarAbrigoJsonDTO.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // ASSERT
+        assertEquals(200, response.getStatus());
+        assertEquals("Abrigo Cadastrado com Sucesso", response.getContentAsString());
+    }
+
+    @Test
+    @DisplayName("Deveria retornar status 400 caso não cadastre abrigos")
+    void cadastrarAbrigosCenario02() throws Exception{
+
+        // ARRANGE
+        CadastrarAbrigoDTO dto = new CadastrarAbrigoDTO("", "", "");
+
+        // ACT
+        var response = mvc.perform(
+                post("/abrigos")
+                        .content(cadastrarAbrigoJsonDTO.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // ASSERT
+        assertEquals(400, response.getStatus());
+    }
+}
---
+++ src/test/java/br/com/alura/adopet/api/controller/PetControllerTest.java
@@ -0,0 +1,7 @@
+package br.com.alura.adopet.api.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetControllerTest {
+
+}
